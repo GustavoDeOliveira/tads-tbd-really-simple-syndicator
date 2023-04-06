5d4fb8490bd14cb2e1e6c0c2501f83c206e35ea5
Gustavo de Oliveira <6ustavo.0liveira@gmail.com>
2023-04-05 23:24:49 -0300

    Initializr ftw

---
+++ rssr/src/main/java/br/edu/ifrs/riogrande/rssr/RssrApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.riogrande.rssr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssrApplication.class, args);
+	}
+
+}
---
+++ rssr/src/test/java/br/edu/ifrs/riogrande/rssr/RssrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.riogrande.rssr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
